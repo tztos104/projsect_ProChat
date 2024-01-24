571dce67c533469ff7d21d9cfc9ba298ad7e8d08
lyj <kasean@nate.com>
2024-01-24 14:14:45 +0900

    first commit

---
+++ src/main/java/Prochat_project/ProchatProjectApplication.java
@@ -0,0 +1,13 @@
+package Prochat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProchatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProchatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/Prochat_project/ProchatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Prochat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProchatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
